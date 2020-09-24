934cce1aa22aeb62785ea3c9207074557be3f2ee
Daniel Candelas <danielcandelas98@gmail.com>
2020-09-24 18:03:01 +0200

    ya no se

--- UD01-Entorno Trabajo/code.js
+++ UD01-Entorno Trabajo/code.js
@@ -5,13 +5,14 @@ tablero[0] = ["_", "_", "_"];
 tablero[1] = ["_", "O", "_"];
 tablero[2] = ["_", "_", "_"];
 
+
 console.log(tablero);
 
 
 function pintoTablero(){
 
-    for (i = 0; i <= 2; i++) {
-        for (j = 0; j <= 2; j++) {
+    for (i = 0; i < tablero.length; i++) {
+        for (j = 0; j < tablero[i].length; j++) {
             var id_casilla = "casilla_" + i + j;
             document.getElementById(id_casilla).value=tablero[i][j];        
         }        
@@ -20,21 +21,24 @@ function pintoTablero(){
 
 function jugada(){
     //leer todos los valor de los inputs y meterlos en tablero
-    var valor;
 
-    for (i = 0; i <= 2; i++) {
-        for (j = 0; j <= 2; j++) {
+    for (i = 0; i < tablero.length; i++) {
+        for (j = 0; j < tablero[i].length; j++) {
             var id_casilla = "casilla_" + i + j;
-            valor = document.getElementById(id_casilla).value;    
-            tablero[i][j] = valor;
-            console.log(tablero[i][j]);        
+            tablero[i][j] = document.getElementById(id_casilla);        
         }        
     }
-    
 }
 
 function tiradaMaquina(){
     //Aleatorio para añadir 0
+    for (i = 0; i < tablero.length; i++) {
+        for (j = 0; j < tablero[i].length; j++) {
+            
+        }        
+    }
+
+
 }
 
 
