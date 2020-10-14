c39b7beafc765f405c1cd54cb720d61ac941c851
Daniel Candelas <danielcandelas98@gmail.com>
2020-10-14 20:35:58 +0200

    kk

--- UD01-Entorno Trabajo/3 en raya v2/tres en raya v2.js
+++ UD01-Entorno Trabajo/3 en raya v2/tres en raya v2.js
@@ -5,6 +5,8 @@ tablero[0] = ["_", "_", "_"];
 tablero[1] = ["_", "O", "_"];
 tablero[2] = ["_", "_", "_"];
 
+var fin_partida = 0;
+
 
 function pintoTablero(){
 
@@ -28,7 +30,75 @@ function pintoTablero(){
 }
 
 
+function jugada(casilla){   // cuado hago click en boton ...
+   
+    console.log(casilla.id);
+    casilla_fila_i=casilla.id.substring(8,9);
+    casilla_fila_j=casilla.id.substring(9,10);
+    console.log("i="+casilla_fila_i+" j="+casilla_fila_j);
+    console.log(tablero[casilla_fila_i][casilla_fila_j]);
+    
+    if(tablero[casilla_fila_i][casilla_fila_j]=="_"){
+        tablero[casilla_fila_i][casilla_fila_j]="x";
+        pinto_tablero();
+        compruebo_filas_columnas_diagonales("x"); // compruebo si ganan las x
+        jugada_maquina();   // turno de la máquina
+        compruebo_filas_columnas_diagonales("o"); // comprueba si ganan los o
+    }
+}
 
 
+function compruebo_filas_columnas_diagonales(jugador){  // compruebo si se gana o empata
+
+        if((tablero[0][0]==tablero[0][1])&&(tablero[0][1]==tablero[0][2])&&(tablero[0][2]==jugador)){ //filas
+            alert (" Ha ganado "+jugador);
+            location.reload();
+        }
+        if((tablero[1][0]==tablero[1][1])&&(tablero[1][1]==tablero[1][2])&&(tablero[1][2]==jugador)){
+            alert (" Ha ganado "+jugador);
+            location.reload();
+        }
+        if((tablero[2][0]==tablero[2][1])&&(tablero[2][1]==tablero[2][2])&&(tablero[2][2]==jugador)){
+            alert (" Ha ganado "+jugador);
+            location.reload();
+        }
+        if((tablero[0][0]==tablero[1][1])&&(tablero[1][1]==tablero[2][2])&&(tablero[2][2]==jugador)){ //diagonales
+            alert (" Ha ganado "+jugador);
+            location.reload();
+        }
+        if((tablero[0][2]==tablero[1][1])&&(tablero[1][1]==tablero[2][0])&&(tablero[2][0]==jugador)){
+            alert (" Ha ganado "+jugador);
+            location.reload();
+        }
+        if((tablero[0][0]==tablero[1][0])&&(tablero[1][0]==tablero[2][0])&&(tablero[2][0]==jugador)){ //columnas
+            alert (" Ha ganado "+jugador);
+            location.reload();
+        }
+        if((tablero[0][1]==tablero[1][1])&&(tablero[1][1]==tablero[2][1])&&(tablero[2][1]==jugador)){
+            alert (" Ha ganado "+jugador);
+            location.reload();
+        }
+        if((tablero[0][2]==tablero[1][2])&&(tablero[1][2]==tablero[2][2])&&(tablero[2][2]==jugador)){
+            alert (" Ha ganado "+jugador);
+            location.reload();
+        }
+        if(fin_partida==4){   // si la maquina lleva 4 turnos, se acaba en empate
+            alert("EMPATE!!!!");
+            location.reload();
+        }
+}
+
+ 
+function jugada_maquina(){  // turno de la máquina
+    jugada_x=Math.floor(Math.random() * 3); //aleatorio 0,1 o 2
+    jugada_y=Math.floor(Math.random() * 3);
+    if(tablero[jugada_x][jugada_y]=="_"){ // si esta vacío, le pongo un 'o'
+        tablero[jugada_x][jugada_y]="o";
+        fin_partida++;
+        pinto_tablero();      
+     }else{    // si no esta vacío, vuelvo a buscar
+        jugada_maquina();
+    }
+}
 
 
