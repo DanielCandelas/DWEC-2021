c2a054dd2f0943a78f20e8061a737677af6ec8d1
Daniel Candelas <danielcandelas98@gmail.com>
2020-10-14 20:05:44 +0200

    ok

--- UD04-Eventos/Ejercicio05/Ejercicio05.js
+++ UD04-Eventos/Ejercicio05/Ejercicio05.js
@@ -2,18 +2,19 @@
 
 function init(){
 
-    document.getElementById("salida").onclick = empezarJuego;
-    
-    function empezarJuego(){
-        document.getElementById("tablero").onmouseout = muestraMensaje;
-        document.getElementById("pared_0").onmouseover = muestraMensaje;
-        document.getElementById("pared_1").onmouseover = muestraMensaje;
-        document.getElementById("pared_2").onmouseover = muestraMensaje;
-    }    
+    document.getElementById("salida").onclick = empezarJuego;        
+}
+
+function empezarJuego(){
+    document.getElementById("tablero").onmouseout = muestraMensaje;
+    document.getElementById("pared_0").onmouseover = muestraMensaje;
+    document.getElementById("pared_1").onmouseover = muestraMensaje;
+    document.getElementById("pared_2").onmouseover = muestraMensaje;
 }
 
 function muestraMensaje() {
     alert('No te salgas del camino color salmon');
+    
 }
 
 
