5699050f0084f953fae726ac59a57f5e1c030376
Daniel Candelas <danielcandelas98@gmail.com>
2020-10-15 14:59:38 +0200

    done

--- UD04-Eventos/Ejercicio05/Ejercicio05.js
+++ UD04-Eventos/Ejercicio05/Ejercicio05.js
@@ -1,20 +1,37 @@
 
+var ms = 0;
+var myIntrval; 
 
 function init(){
 
-    document.getElementById("salida").onclick = empezarJuego;        
+    document.getElementById("salida").onmouseover = empezarJuego;
+    
 }
 
-function empezarJuego(){
-    document.getElementById("tablero").onmouseout = muestraMensaje;
+function empezarJuego(){    
+
+    myIntrval = setInterval(intervalo, 100); 
+
     document.getElementById("pared_0").onmouseover = muestraMensaje;
     document.getElementById("pared_1").onmouseover = muestraMensaje;
-    document.getElementById("pared_2").onmouseover = muestraMensaje;
+    document.getElementById("pared_2").onmouseover = muestraMensaje;    
+    document.getElementById("final").onmouseover = acabarJuego;
 }
 
 function muestraMensaje() {
     alert('No te salgas del camino color salmon');
-    
+    location.reload();
+}
+
+function acabarJuego() {
+    clearInterval(myIntrval);
+    var totalTime = ms/10;
+    alert("Has ganado, has tardado " + totalTime + " segundos");
+    location.reload();
+}
+
+function intervalo(){
+    ms++;
 }
 
 
