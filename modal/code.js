fecc09e1c8e08cd836d86614e4036cf156d6725f
Daniel Candelas <danielcandelas98@gmail.com>
2020-11-06 17:00:59 +0100

    ok

---
+++ modal/code.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	$("#caja-modal1").hide();
+	$("#caja-modal2").hide();
+	
+
+	$("#ver-caja1").click(function(){
+		$("#caja-modal1").show();
+	})
+	$("#ver-caja2").click(function(){
+		$("#caja-modal2").show();
+	})
+	
+	$("#ocultar-caja").click(function(){
+		$("#caja-modal1").hide();
+	})
+
+	$("#ocultar-caja2").click(function(){
+		$("#caja-modal2").hide();
+	})
+
+	
+
+
+});
+
+
