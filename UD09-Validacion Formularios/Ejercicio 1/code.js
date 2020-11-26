8c19a111d7eb8d316b5166a22d0348969318c6b9
Daniel Candelas <danielcandelas98@gmail.com>
2020-11-26 17:01:47 +0100

    ok

---
+++ UD09-Validacion Formularios/Ejercicio 1/code.js
@@ -0,0 +1,18 @@
+
+
+
+function validar(){
+    var campo = document.getElementById("nombre");
+
+    if(campo == null){
+        alert("El nombre esta vacio");
+        return false;
+    }
+
+    var edad = document.getElementById("edad");
+
+    if(edad == null || edad.minLength < 0  || edad.maxLength > 100 || isNaN(edad)){
+        alert("La edad esta vacia, es manor de 0 o mayor de 100, o no es un numero");
+        return false;
+    }
+}
