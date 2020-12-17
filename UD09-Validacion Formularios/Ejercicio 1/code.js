60fa83a3051bb24e0042a8442eeab765ecd6f235
Daniel Candelas <danielcandelas98@gmail.com>
2020-12-17 18:18:50 +0100

    ok

--- UD09-Validacion Formularios/Ejercicio 1/code.js
+++ UD09-Validacion Formularios/Ejercicio 1/code.js
@@ -3,19 +3,20 @@
 
 function validar(){
     var campo = document.getElementById("nombre");
-
-    if(campo == null){
+    if(campo == null || campo.length == 0 || /^\s+$/.test(campo)){
         alert("El nombre esta vacio");
         return false;
     }
 
     var edad = document.getElementById("edad");
-
     if(edad == null || edad.minLength < 0  || edad.maxLength > 100 || isNaN(edad)){
         alert("La edad esta vacia, es manor de 0 o mayor de 100, o no es un numero");
         return false;
     }
 
-
-
+    var email = document.getElementById("correo").value;
+    if( !(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(email)) ) {
+        alert("El correo no esta escrito correctamente");
+        return false;
+    }
 }
